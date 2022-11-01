b04012e36b3556116c4dcce4f531914fa61a4156
intinek <102937646+intinek@users.noreply.github.com>
2022-10-31 23:33:28 -0300

    Add files via upload

---
+++ intie/src/main/java/com/back/intie/IntieApplication.java
@@ -0,0 +1,13 @@
+package com.back.intie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntieApplication.class, args);
+	}
+
+}
---
+++ intie/src/main/java/com/back/intie/Security/Service/RolService.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.back.intie.Security.Service;
+
+import com.back.intie.Security.Entity.Rol;
+import com.back.intie.Security.Enums.RolNombre;
+import com.back.intie.Security.Repository.iRolRepository;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class RolService {
+    @Autowired
+    iRolRepository irolRepository;
+    
+    public Optional<Rol> getByRolNombre(RolNombre rolNombre){
+        return irolRepository.findByRolNombre(rolNombre);
+    }
+    
+    public void save(Rol rol){
+        irolRepository.save(rol);
+    }
+}
