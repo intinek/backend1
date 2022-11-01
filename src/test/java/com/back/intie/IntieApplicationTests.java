9aa06255c86196f1c6d4261af8b4e90c6354de84
intinek <echeverriainti@hotmail.com>
2022-10-31 21:42:26 -0300

    segunda prueba

---
+++ src/test/java/com/back/intie/IntieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.intie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
